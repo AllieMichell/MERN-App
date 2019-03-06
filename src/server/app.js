38a84f655ae2af21401d5c29e01a0d388a067ab4
Allie <allie_michell@hotmail.com>
2019-03-06 12:28:14 -0600

    MERN-App

---
+++ src/server/app.js
@@ -0,0 +1,37 @@
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+// Mongoose conection
+mongoose.connect('mongodb://localhost:27017/Find-Eat', (err) => {
+  if (err) throw err;
+  console.log('Successfully conected');
+});
+// view engine setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('dist'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+module.exports = app;
